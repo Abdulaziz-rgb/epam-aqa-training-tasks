84bfb58a438c8eed52a4cac2aaa02369a9a67cc6
Abdulaziz-rgb <shokirovabdulaziz19@gmail.com>
2023-01-04 21:23:04 +0500

    added task 4 solution

---
+++ Tasks/Entities/Transmission.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1.Entities;
+
+public class Transmission
+{
+    // transmission types can be of manual or automatic
+    public string Type { get; set; }
+    
+    public int NumberOfGears { get; set; }
+    
+    public string Manufacturer { get; set; }
+}
---
+++ Tasks/Entities/Vehicles/PassengerCar.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1.Entities.Vehicles;
+
+public class PassengerCar : Vehicle
+{
+    public PassengerCar(string name, Engine engine, Chassis chassis, Transmission transmission) : base(name, engine, chassis, transmission)
+    {
+    }
+}
