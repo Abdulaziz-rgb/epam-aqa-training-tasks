53272eb1eb233925ce5adcc2a02e5fab8dbc9dca
Abdulaziz-rgb <shokirovabdulaziz19@gmail.com>
2023-02-20 12:38:13 +0500

    added solution for unit test frameworks task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using ConsoleApp1.task_1;
+
+Console.Write("Enter a word to check: ");
+var input = Console.ReadLine();
+        
+if (input.Length == 0 || input.Length == 1 ) Console.Write("Value cannot be one string or  null. Please re-run the program with correct credential!");
+
+var sol = new Solution();
+Console.WriteLine(sol.FindConsecutiveIdenticalLetters(input));
---
+++ Tasks/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp1.task_1;
+
+public  class Solution
+{
+    public  int FindConsecutiveIdenticalLetters(string words)
+    {
+        var LatinLetters = "abcdefghiklmnopqrstvxyz";
+        words.ToLower();
+        var result = BaseHandler(words, LatinLetters);
+        return result;
+    }
+
+    public  int FindConsecutiveIdenticalDigits(string words)
+    {
+        var digits = "0123456789";
+        var result = BaseHandler(words,digits);
+        return result;
+    }
+
+    private  int BaseHandler(string words, string testSet)
+    {
+        int count = 1;
+        int maxCount = 1;
+
+        for (int i = 1; i < words.Length; i++)
+        {
+            if (testSet.Contains(words[i]) && words[i] == words[i-1])
+            {
+                count++;
+                if (count > maxCount) maxCount = count;
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+        return maxCount;
+    }
+}
+
